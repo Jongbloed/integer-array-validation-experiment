d37f6fa78645a85df74fa4ce723c814160db43cd
Jongbloed <erik.jongbloed@gmail.com>
2020-02-25 23:31:55 +0100

    init

---
+++ IntegerArrayValidation/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using IntegerArrayValidation.Models;
+
+namespace IntegerArrayValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IntegerArrayInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.Answer = "Valid";
+            }
+            else
+            {
+                model.Answer = "Nope!";
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ IntegerArrayValidation/Models/IntegerArrayInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegerArrayValidation.Models
+{
+    public class IntegerArrayInputModel
+    {
+        [MinLength(1)]
+        [RegularExpression(@"^\[[0-9]+(?:,\s*[0-9]+)*\]$")]
+        [DisplayName("Integer array")]
+        public string InputText { get; set; }
+        public string Answer { get; set; }
+    }
+}
