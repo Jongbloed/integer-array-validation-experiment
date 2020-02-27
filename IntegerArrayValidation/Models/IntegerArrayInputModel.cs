86ff75d1103dac1947611da6916d4ea1e08b1e04
Jongbloed <erik.jongbloed@gmail.com>
2020-02-27 22:05:33 +0100

    implement 2 versions of solution

--- IntegerArrayValidation/Models/IntegerArrayInputModel.cs
+++ IntegerArrayValidation/Models/IntegerArrayInputModel.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IntegerArrayValidation.Models
 {
@@ -11,7 +7,7 @@ namespace IntegerArrayValidation.Models
     {
         [Required]
         [RegularExpression(
-            @"^\[[0-9]+(?:,\s*[0-9]+)*\]$", 
+            @"^\[\s*[0-9]+\s*(?:,\s*[0-9]+)*\]$", 
             ErrorMessage = "Please enter an array of integers in the format \"[x,y]\""
         )]
         [DisplayName("Integer array")]
---
+++ IntegerArrayValidation/Triplicate.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace IntegerArrayValidation
+{
+    public static class Triplicate
+    {
+        public static int[] DetermineLinq(int[] input)
+        {
+            var output = input
+                .GroupBy(x => x)
+                .Where(x => x.Take(3).Count() >= 3)
+                .Select(x => x.Key)
+                .OrderByDescending(x => x)
+                .ToArray();
+            return output;
+        }
+
+        public static int[] DetermineFor(int[] input)
+        {
+            var map = new Dictionary<int, int>(input.Length);
+            var triplicates = new SortedSet<int>(new ReverseComparer());
+            foreach (var x in input)
+            {
+                if (!map.TryAdd(x, 1))
+                {
+                    if (++map[x] == 3)
+                    {
+                        triplicates.Add(x);
+                    }
+                }
+            }
+            return triplicates.ToArray();
+        }
+
+        class ReverseComparer : IComparer<int>
+        {
+            public int Compare(int x, int y)
+            {
+                return y.CompareTo(x);
+            }
+        }
+    }
+}
